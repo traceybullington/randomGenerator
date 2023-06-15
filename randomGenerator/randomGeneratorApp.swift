2502d8f8d18c81c49f94d0adafaaacfcee841bf2
Tracey Bullington <traceylbullington@gmail.com>
2023-06-15 13:45:47 -0500

    Initial Commit

---
+++ randomGenerator/randomGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomGeneratorApp.swift
+//  randomGenerator
+//
+//  Created by scholar on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct randomGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
